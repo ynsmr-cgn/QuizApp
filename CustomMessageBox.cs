5bd7c641362ee4a1cf5d2f29723e4043cf2e6d6c
Yunus Emre Çağan <123986840+ynsmr-cgn@users.noreply.github.com>
2024-05-12 16:47:10 +0300

    Update CustomMessageBox.cs

--- CustomMessageBox.cs
+++ CustomMessageBox.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,6 +18,7 @@ namespace QuizApp
             InitializeComponent();
         }
 
+        // Constants and WinAPI function declarations for form dragging.
         public const int WM_NCLBUTTONDOWN = 0xA1;
         public const int HT_CAPTION = 0x2;
         [DllImportAttribute("user32.dll")]
@@ -25,6 +26,7 @@ namespace QuizApp
         [DllImportAttribute("user32.dll")]
         public static extern bool ReleaseCapture();
 
+        // Allows the form to be moved by clicking and dragging on a specific area.
         private void moveableBar_MouseMove(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
@@ -34,26 +36,31 @@ namespace QuizApp
             }
         }
 
+        // Minimizes the form when the minimize button is clicked.
         private void MinimizeButton_Click(object sender, EventArgs e)
         {
             WindowState = FormWindowState.Minimized;
         }
 
+        // Closes the form when the close button is clicked.
         private void CloseButton_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
+        // Sets the dialog result to OK when the OK button is clicked.
         private void btnOk_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.OK;
         }
 
+        // Closes the form when the close button is clicked.
         private void CloseButton_Click_1(object sender, EventArgs e)
         {
             this.Close();
         }
 
+        // Minimizes the form when the minimize button is clicked.
         private void MinimizeButton_Click_1(object sender, EventArgs e)
         {
             WindowState |= FormWindowState.Minimized;
